e65be3e1f910f403213e161bbe58fdd222cb3038
karthi861 <74948463+karthi861@users.noreply.github.com>
2021-06-16 20:57:13 +0530

    Add files via upload

---
+++ handlers/feed_handler.py
@@ -0,0 +1,39 @@
+import json
+from flask import Blueprint, request
+from services.feed_service import FeedService
+
+
+FEED = Blueprint(
+    'feed',
+    __name__,
+    url_prefix='/api/v1'
+
+)
+
+
+@FEED.route('/users', methods=['POST'])
+def create_user():
+    params = request.get_json()
+    FeedService.add_user(params)
+    return json.dumps({
+        'Message': 'User {name} {title} {content} inserted.'
+    })
+
+
+@FEED.route('/users', methods=['GET'])
+def get_all_user():
+    users = FeedService.get_all_users()
+    return json.dumps({'users': users})
+
+
+@FEED.route('/user/<user_id>', methods=['PUT'])
+def update_user():
+    params = request.get_json()
+    FeedService.update_user(params)
+    return json.dumps(({'message': 'user updated successfully'}))
+
+
+@FEED.route('/user/<id>', methods=['DELETE'])
+def delete_user(id):
+    FeedService.delete_user(id)
+    return {"message": f"User successfully deleted."}
