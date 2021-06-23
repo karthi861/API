c989916ca0eab6879a1f9ae6904967506146e6bc
Karthikeyan J <74948463+karthi861@users.noreply.github.com>
2021-06-23 20:20:39 +0530

    Update init.py

--- init.py
+++ init.py
@@ -1,13 +1,23 @@
+import logging
 from flask import Flask
 from config import CONFIG
 from handlers.feed_handler import FEED
+from handlers.authentication_handler import AUTHENTICATION
 from models import db
+
+logging.basicConfig(
+    format='%(levelname)-8s %(asctime)s,%(msecs)d  [%(filename)s:%(lineno)d] %(message)s',
+    datefmt='%Y-%m-%d:%H:%M:%S',
+    level=logging.INFO
+)
 
 
 def init_app():
     app = Flask(__name__)
+    app.secret_key = ['thisissecret']
     app.config['SQLALCHEMY_DATABASE_URI'] = CONFIG['SQLALCHEMY_DATABASE_URI']
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = CONFIG['SQLALCHEMY_TRACK_MODIFICATIONS']
     db.init_app(app)
     app.register_blueprint(FEED)
+    app.register_blueprint(AUTHENTICATION)
     return app
